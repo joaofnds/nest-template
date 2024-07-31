573ba311e8313d13019c0d87cd2d2db27e748bad
João Fernandes <joaofnds@joaofnds.com>
2024-07-30 21:56:01 -0300

    chore: update deps

--- src/pokeapi/schemas/pokemon.schema.ts
+++ src/pokeapi/schemas/pokemon.schema.ts
@@ -2,7 +2,7 @@ import { Schema } from "@effect/schema";
 
 export const PokemonSchema = Schema.Struct({
 	id: Schema.Number.pipe(Schema.positive()),
-	name: Schema.String.pipe(Schema.nonEmpty()),
+	name: Schema.String.pipe(Schema.nonEmptyString()),
 	is_default: Schema.Boolean,
 	height: Schema.Number.pipe(Schema.positive()),
 	weight: Schema.Number.pipe(Schema.positive()),
