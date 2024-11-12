5f01baeef23ec997bcb5565699a206a7731a4983
João Fernandes <joaofnds@joaofnds.com>
2024-11-12 19:43:06 -0300

    config: add redis family

--- src/redis/config.ts
+++ src/redis/config.ts
@@ -2,12 +2,16 @@ import { z } from "zod";
 
 export const redisConfigSchema = z.object({
 	url: z.string().startsWith("redis://").url(),
+	family: z.union([z.literal(4), z.literal(6)]),
 });
 
 export class RedisConfig {
-	constructor(readonly url: string) {}
+	constructor(
+		readonly url: string,
+		readonly family: number,
+	) {}
 
 	static fromPlain(config: z.infer<typeof redisConfigSchema>) {
-		return new RedisConfig(config.url);
+		return new RedisConfig(config.url, config.family);
 	}
 }
